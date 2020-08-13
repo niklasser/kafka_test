fc025221504c454cee6aedb257a692078a337fe5
Niklas Sänger <uqdlm@student.kit.edu>
2020-08-13 16:03:13 +0200

    update

---
+++ scripts/helper.py
@@ -0,0 +1,33 @@
+import argparse
+import kafka
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--host", help="Change cluster host (default is localhost:9092")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-list", help="Lists topics",action="store_true")
+group.add_argument("-add", help="Adds topic. Takes <name> <partitions> <replication factor>", nargs=3)
+group.add_argument("-delete", help="Deletes topic. Takes <Topic name>")
+args = parser.parse_args()
+
+host = 'localhost:9092'
+if args.host:
+    host = args.host
+
+if args.list:
+    # To consume latest messages and auto-commit offsets
+    consumer = kafka.KafkaConsumer(bootstrap_servers=[host])
+    topics = consumer.topics()
+    for topic in topics:
+        print(topic)
+elif args.add:
+    admin = kafka.KafkaAdminClient(bootstrap_servers=[host])
+    topics=[]
+    topics.append(kafka.admin.NewTopic(name=args.add[0], num_partitions=int(args.add[1]), replication_factor=int(args.add[2])))
+    res = admin.create_topics(new_topics=topics)
+    print(res)
+elif args.delete:
+    admin = kafka.KafkaAdminClient(bootstrap_servers=[host])
+    topics=[]
+    topics.append(args.delete)
+    res = admin.delete_topics(topics)
+    print(res)
---
+++ scripts/sensor_producer.py
@@ -0,0 +1,23 @@
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+import numpy.random as np
+import time
+import json
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+#Sending random sensor data
+while True:
+    msg = {"temp":str(np.randint(-20, 42)), "time":str(time.time_ns()), "co2":str(np.randint(1,1000))}
+    producer.send("sensordata", msg)
+    print("Send sensor data: " + str(msg))
+    time.sleep(np.randint(1, 10))
+
+def on_send_success(record_metadata):
+    print(record_metadata.topic)
+    print(record_metadata.partition)
+    print(record_metadata.offset)
+
+def on_send_error(excp):
+    log.error('I am an errback', exc_info=excp)
+    # handle exception
+
